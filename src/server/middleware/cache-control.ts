dcaf1d08010fc3d5a139044bf6db7418580a3609
Rafhael <rafamlf.2011@gmail.com>
2023-06-09 15:46:14 -0300

    cache policy change

--- src/server/middleware/cache-control.ts
+++ src/server/middleware/cache-control.ts
@@ -1,12 +1,12 @@
 export default defineEventHandler(event => {
   const res = event.node.res;
   const year = 31536000; // 365 days
-  const hour = 60 * 60; // 1 hour
+  const hour = 60 * 60 * 30; // 30 days
   const url = event.node.req.url;
-  const method = event.node.req.method;
+  // const method = event.node.req.method;
 
-	if (url && method === 'GET') {
-    const maxage = url.match(/(.+)\.(jpg|jpeg|gif|css|png|js|ico|svg|xml|mjs)/) ? year : hour;
+	if (url) {
+    const maxage = url.match(/(.+)\.(jpg|jpeg|gif|css|png|js|ico|svg|xml|svg+xml|mjs)/) ? year : hour;
 		res.setHeader('Cache-Control', `max-age=${maxage}`);
   }
 });
