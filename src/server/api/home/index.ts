d1c638b7bae3242ab26168567e2e41d3290bd07b
Rafhael <rafamlf.2011@gmail.com>
2023-06-25 21:35:19 -0300

    added cache to home endpoint

--- src/server/api/home/index.ts
+++ src/server/api/home/index.ts
@@ -2,7 +2,7 @@ import supabase from "~~/src/config/supabase";
 // import { DashboardForm } from "~~/src/types/dashboard";
 // import { HomePage } from "~~/src/models/homePageObject";
 
-export default defineEventHandler(async() => {
+export default defineCachedEventHandler(async() => {
 	try {	
 		const { data, error } = await supabase.from('HOME_PAGE').select('*');
 		if (error) throw error;
@@ -21,4 +21,6 @@ export default defineEventHandler(async() => {
 	} catch (error) {
 		throw error;
 	}
+}, {
+	maxAge: 60 * 60 * 24, // 24 hour
 });
--- src/server/middleware/cache-control.ts
+++ src/server/middleware/cache-control.ts
@@ -1,7 +1,7 @@
 export default defineEventHandler(event => {
   const res = event.node.res;
   const year = 31536000; // 365 days
-  const days = 60 * 60 * 1; // 5 days
+  const days = 60 * 60 * 24; // 1 day
   const url = event.node.req.url;
   // const method = event.node.req.method;
 
