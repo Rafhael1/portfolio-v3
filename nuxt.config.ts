3450f8a63180976e135f2981607a351894bd75ca
Rafhael <rafamlf.2011@gmail.com>
2023-09-12 09:18:57 -0300

    ajuste hotjar

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,19 +23,7 @@ export default defineNuxtConfig({
 				lang: 'en',
 
 			},
-			script: [
-				`<!-- Hotjar Tracking Code for https://rafhaelmarques.me -->
-				<script>
-						(function(h,o,t,j,a,r){
-								h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-								h._hjSettings={hjid:3649822,hjsv:6};
-								a=o.getElementsByTagName('head')[0];
-								r=o.createElement('script');r.async=1;
-								r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-								a.appendChild(r);
-						})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-				</script>`,
-			],
+			script: [],
 			meta: [
 				{ charset: 'utf-8' },
 				{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -72,7 +60,7 @@ export default defineNuxtConfig({
 	nitro: {
     compressPublicAssets: true,
 		prerender: {
-			routes: ['/', '/home'],
+			routes: ['/'],
 		}
   },
   vite: {
