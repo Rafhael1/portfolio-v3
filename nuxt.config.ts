111b312c620f8c1db73c90c47758473d8978cd20
Rafhael <rafhaelfreitas@lojadomecanico.com.br>
2023-12-23 23:23:07 -0300

    cache adjust

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,51 +2,51 @@
 import svgLoader from 'vite-svg-loader'
 
 export default defineNuxtConfig({
-	routeRules: {
-		'/': { prerender: true },
-		'/api/**': { cors: true },
-	},
+								routeRules: {
+																'/': { prerender: true },
+																'/api/**': { cors: true },
+								},
   modules: [
     '@nuxtjs/supabase',
     '@pinia/nuxt',
     'vite-plugin-vue-type-imports/nuxt',
-	],
-	runtimeConfig: {
+								],
+								runtimeConfig: {
     SUPABASE_URL: process.env.SUPABASE_URL,
     SUPABASE_KEY: process.env.SUPABASE_KEY		
-	},
-	srcDir: 'src/',
-	app: {		
-		head: {
-			title: 'Rafhael Marques',
-			htmlAttrs: {
-				lang: 'en',
+								},
+								srcDir: 'src/',
+								app: {		
+																head: {
+																								title: 'Rafhael Marques',
+																								htmlAttrs: {
+																																lang: 'en',
 
-			},
-			script: [],
-			meta: [
-				{ charset: 'utf-8' },
-				{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
-				{ hid: 'description', name: 'description', content: 'Rafhael Marques portfolio website.' },
-				//{ name: 'format-detection', content: 'telephone=no' },
-				{ hid: 'og:locale', property: 'og:locale', content: 'en_US' },
-				{ hid: 'og:locale:alternate', property: 'og:locale:alternate', content: 'pt_BR' },				
-				// index
-				{ hid: 'og:type', property: 'og:type', content: 'website' },
-				{ hid: 'og:title', property: 'og:title', content: 'Rafhael Marques de Lima Freitas' },
-				{ hid: 'og:description', property: 'og:description', content: 'Rafhael Marques portfolio website. Typescript, Javascript, Node.js, Nest.js, React, Angular, Vue, Nuxt' },
-				{ hid: 'og:url', property: 'og:url', content: 'https://rafhaelmarques.me' },
-				{ hid: 'og:site_name', property: 'og:site_name', content: 'Rafhael Marques de Lima Freitas' },
-				// robot meta tag
-				{ hid: 'robots', name: 'robots', content: 'index, follow' },				
-			],
-			link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-		},
-	},
+																								},
+																								script: [],
+																								meta: [
+																																{ charset: 'utf-8' },
+																																{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+																																{ hid: 'description', name: 'description', content: 'Rafhael Marques portfolio website.' },
+																																//{ name: 'format-detection', content: 'telephone=no' },
+																																{ hid: 'og:locale', property: 'og:locale', content: 'en_US' },
+																																{ hid: 'og:locale:alternate', property: 'og:locale:alternate', content: 'pt_BR' },				
+																																// index
+																																{ hid: 'og:type', property: 'og:type', content: 'website' },
+																																{ hid: 'og:title', property: 'og:title', content: 'Rafhael Marques de Lima Freitas' },
+																																{ hid: 'og:description', property: 'og:description', content: 'Rafhael Marques portfolio website. Typescript, Javascript, Node.js, Nest.js, React, Angular, Vue, Nuxt' },
+																																{ hid: 'og:url', property: 'og:url', content: 'https://rafhaelmarques.me' },
+																																{ hid: 'og:site_name', property: 'og:site_name', content: 'Rafhael Marques de Lima Freitas' },
+																																// robot meta tag
+																																{ hid: 'robots', name: 'robots', content: 'index, follow' },				
+																								],
+																								link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+																},
+								},
   css: ['assets/css/main.css', '@fortawesome/fontawesome-svg-core/styles.css'],
   postcss: {
     plugins: {
-			cssnano: {
+																								cssnano: {
         preset: ['default', {
           discardComments: {
             removeAll: true,
@@ -57,11 +57,11 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
-	nitro: {
+								nitro: {
     compressPublicAssets: true,
-		prerender: {
-			routes: ['/'],
-		}
+																prerender: {
+																								routes: ['/'],
+																}
   },
   vite: {
     plugins: [
--- src/server/middleware/cache-control.ts
+++ src/server/middleware/cache-control.ts
@@ -1,9 +1,7 @@
 export default defineEventHandler(event => {
   const res = event.node.res;
-  // const year = 31536000; // 365 days
-  const days = 60 * 60 * 24; // 1 day
+  const days = 60 * 60 * 24 * 1; // 30 days
   const url = event.node.req.url;
-  // const method = event.node.req.method;
 
 	if (url) {
     const maxage = days;
