19f86aa87dc0e607e2a5df15789e173d8ba57167
Rafhael <rafhaelfreitas@lojadomecanico.com.br>
2024-02-07 09:55:32 -0300

    removed nodemailer

--- src/server/api/email/index.post.ts
+++ src/server/api/email/index.post.ts
@@ -1,5 +1,3 @@
-import nodeMailer from "nodemailer";
-
 interface Body {
 	name: string;
 	email: string;
@@ -12,40 +10,14 @@ export default defineEventHandler(async(event) => {
 	if (!body.email || !body.subject || !body.message) {
 		throw new Error("Missing required fields");
 	}
-	
-	const userEmail = process.env.GMAIL_NODEMAILER_USER;
-	const userPassword = process.env.GMAIL_NODEMAILER_PASS;
 
-	const transporter = nodeMailer.createTransport({
-		service: "smtp.mailgun.org",
-		port: 587,
-		auth: {
-			user: userEmail,
-			pass: userPassword
-		}
+	await $fetch(`${process.env.PORTFOLIO_JAVA_API_URL}/contact/send-email`, {
+		method: "POST",
+		body: JSON.stringify({
+			name: body.name,
+			email: body.email,
+			subject: body.subject,
+			message: body.message + "\n\n" + "Sent Through Java",
+		})
 	});
-
-	const sendByJavaApi = async () => {
-		try {
-			await $fetch(`${process.env.PORTFOLIO_JAVA_API_URL}/contact/send-email`, {
-				method: "POST",
-				body: JSON.stringify({
-					name: body.name,
-					email: body.email,
-					subject: body.subject,
-					message: body.message + "\n\n" + "Sent Through Java",
-				})
-			})
-		} catch (err) {
-			console.error(err);
-		}
-	}
-
-	transporter.sendMail({
-		from: userEmail,
-		to: process.env.RECIPIENT_EMAIL,
-		subject: `${body.email} - ${body.subject}`,
-		text: body.message + "\n\n" + "Sent Through Node",
-	});
-	await sendByJavaApi();
 });
